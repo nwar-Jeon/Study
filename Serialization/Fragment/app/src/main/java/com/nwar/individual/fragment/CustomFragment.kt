809e7dd81fea87c861168df77b8336d65a2efb4e
nwar-Jeon <nwar0825@naver.com>
2019-05-09 16:35:54 +0900

    .

---
+++ Serialization/Fragment/app/src/main/java/com/nwar/individual/fragment/CustomFragment.kt
@@ -0,0 +1,12 @@
+package com.nwar.individual.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CustomFragment() : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = inflater.inflate(R.layout.fragment_custom,container)
+    
+}
