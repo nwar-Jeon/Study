5a35956d3b24437e8b5b9a536aa2737eb662e4fb
nwar-Jeon <nwar0825@naver.com>
2019-07-22 17:46:07 +0900

    .

--- Android/netWorking/socket/app/src/main/java/com/nwar/individual/socket/MainActivity.kt
+++ Android/netWorking/socket/app/src/main/java/com/nwar/individual/socket/MainActivity.kt
@@ -2,9 +2,14 @@ package com.nwar.individual.socket
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.net.ServerSocket
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +18,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var serverTextView : TextView
     lateinit var sendButton : Button
     lateinit var serverButton : Button
+    val handler = Handler()
+    val portNumber = 5001
+    val host = "10.0.2.2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +30,63 @@ class MainActivity : AppCompatActivity() {
         sendTextView = findViewById(R.id.send_textView)
         serverButton = findViewById(R.id.server_button)
         serverTextView = findViewById(R.id.server_textView)
+
+        sendButton.setOnClickListener {
+            val str = editText.text.toString()
+            Thread{
+                send(str)
+            }.start()
+        }
+        serverButton.setOnClickListener {
+            Thread{
+                startServer()
+            }.start()
+        }
+    }
+
+    fun printClientLog(data : String){
+        handler.post{
+            sendTextView.append(data + "\n")
+        }
+    }
+
+    fun printServerLog(data : String){
+        handler.post {
+            serverTextView.append(data + "\n")
+        }
+    }
+
+    fun send(string : String){
+        val socket = Socket(host, portNumber)
+        printClientLog("서버 연결함.")
+        val outStream = ObjectOutputStream(socket.getOutputStream())
+        outStream.writeObject(string)
+        outStream.flush()
+        printClientLog("데이터 전송함.")
+
+        val inStream = ObjectInputStream(socket.getInputStream())
+        printClientLog("서버로부터 받음 : " + inStream.readObject())
+        socket.close()
+    }
+
+    fun startServer(){
+        val server = ServerSocket(portNumber)
+        printServerLog("서버 시작함 : $portNumber")
+
+        while(true){
+            val socket = server.accept()
+            val clientHost = socket.localAddress
+            val clientPort = socket.port
+            printServerLog("클라이언트 연결 : $clientHost : $clientPort")
+
+            val inStream = ObjectInputStream(socket.getInputStream())
+            val obj = inStream.readObject()
+            printClientLog("데이터 받음 : " + obj)
+
+            val outStream = ObjectOutputStream(socket.getOutputStream())
+            outStream.writeObject("$obj from server")
+            outStream.flush()
+            printServerLog("데이터 전송함.")
+        }
     }
 }
